5787149f09685550e6e711bdf4e810c526581e87
WenZheng Li <me@wenzheng.li>
2020-12-10 10:40:01 +0900

    bid_optimization wip

--- bid_optimization/bid_optimizer.py
+++ bid_optimization/bid_optimizer.py
@@ -5,7 +5,7 @@ class BidOptimizer:
     def __init__(self, caller, cost_function):
         self.caller = caller
         self.cost_function = cost_function
-        self.won_auctions = {}
+        self.participating_auctions = {}
         self.on_going_auctions = {}
         self.utilities = {}
         pass
@@ -15,43 +15,50 @@ class BidOptimizer:
 
     def evaluate_and_bid(self, recompute_utilities=False):
         best_auctions = [None, None]
+        won_auctions = []
         for auction_id, auction in on_going_auctions.items():
             # delete expired auctions
             if not auction.accepting_bids():
+                if auction.get_contractor() == self.caller:
+                    won_auctions.append(auction)
                 del self.on_going_auctions[auction_id]
                 del self.utilities[auction_id]
                 continue
             if recompute_utilities:
-                self.utilities[auction_id] = self.utility_function(auction)
+                self.utilities[auction_id] = self.utility_function(
+                    auction, self.participating_auctions)
             # reject negative utilities
             if self.utilities[auction_id] <= 0:
                 continue
             # find the highest utility auctions
-            if best_auction[0] is None or self.utilities[
-                    auction_id] > self.utilities[best_auction[0]]:
-                best_auction[0] = auction_id
-            elif best_auction[1] is None or self.utilities[
-                    auction_id] > self.utilities[best_auction[1]]:
-                best_auction[1] = auction_id
-            # bid on the best auction at a price of decision boundary between second best
-            if best_auction[0] is not None:
-                # TODO handle bidding errors
-                self.on_going_auctions[best_auction[0]].bid(
-                    self.caller, self.utilities[best_auction[0]] -
-                    self.utilities[best_auction[1]] -
-                    self.on_going_auctions[best_auction[0]].get_current_pay)
-                self.won_auctions[best_auction[0]] = self.on_going_auctions[
-                    best_auction[0]]
-                del self.on_going_auctions[best_auction[0]]
-                return self.won_auctions[best_auction[0]]
+            if best_auctions[0] is None or self.utilities[
+                    auction_id] > self.utilities[best_auctions[0]]:
+                best_auctions[0] = auction_id
+            elif best_auctions[1] is None or self.utilities[
+                    auction_id] > self.utilities[best_auctions[1]]:
+                best_auctions[1] = auction_id
+        # bid on the best auction at a price of decision boundary between second best
+        if best_auctions[0] is not None:
+            best_auction = self.on_going_auctions[best_auctions[0]]
+            # TODO handle bidding errors
+            best_auction.bid(
+                self.caller,
+                best_auction.get_current_pay() -
+                self.utilities[best_auctions[0]] +
+                self.utilities[best_auctions[1]])
+            self.participating_auctions[best_auctions[0]] = best_auction
+        return won_auctions
 
     def on_auction_update(self, auction_id, auction):
-        ''' creation, bid, extend, cancel, confirm'''
-        if not auction.accepting_bids():
-            return
-        # update utility cache only if bid was updated
+        '''possible events: creation, bid, cancel,    extend, confirm'''
+        # check if participating auction was out bid
+        if auction_id in self.participating_auctions and auction.get_contractor(
+        ) != self.caller:
+            del self.participating_auctions[auction_id]
+        # update utility only if bid was updated
         if auction_id not in self.on_going_auctions or auction.get_current_bid(
         ) != self.on_going_auctions[auction_id].get_current_bid():
-            self.utilities[auction_id] = self.utility_function(auction)
-
+            self.utilities[auction_id] = self.utility_function(
+                auction, self.participating_auctions)
+        # store updated auction
         self.on_going_auctions[auction_id] = auction
