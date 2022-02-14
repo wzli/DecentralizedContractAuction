d01fdff6140c9451391c5c8eb11330c4e3b412dc
WenZheng Li <me@wenzheng.li>
2022-02-14 23:03:29 +0900

    wip

--- dta_agent/src/main.rs
+++ dta_agent/src/main.rs
@@ -4,7 +4,6 @@ use subxt::{ClientBuilder, DefaultConfig, DefaultExtra, PairSigner};
 
 #[subxt::subxt(runtime_metadata_path = "metadata.scale")]
 pub mod runtime {}
-use runtime::runtime_types::frame_support::storage::bounded_vec::BoundedVec;
 
 impl fmt::Display for runtime::runtime_types::sp_runtime::DispatchError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -16,6 +15,8 @@ impl fmt::Display for runtime::runtime_types::sp_runtime::DispatchError {
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
+
+    //let signer = PairSigner::new(AccountKeyring::Alice.pair());
 
     let signer = PairSigner::<DefaultConfig, DefaultExtra<DefaultConfig>, _>::new(
         AccountKeyring::Alice.pair(),
@@ -42,13 +43,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             10000,
             5000,
             blocknum + 1000,
-            BoundedVec(vec![0xFF; 8]),
+            vec![0xFF; 8],
         )
         .sign_and_submit_then_watch(&signer)
         .await?
         .wait_for_finalized_success()
         .await;
 
+    let events = tx_result.map_err(|e| match e {
+        subxt::Error::Runtime(re) => panic!("{}", re.inner()),
+        e => e,
+    })?;
+
+    for e in events.as_slice() {
+        println!("{e:?}");
+    }
+
+    let created_event = events
+        .find_first_event::<runtime::task_auction::events::Created>()?
+        .unwrap();
+    println!("{created_event:#?}");
+
+    let (owner, nonce) = created_event.auction_key.clone();
+    let auction = api
+        .storage()
+        .task_auction()
+        .auctions(owner, nonce, None)
+        .await?;
+
+    println!("{auction:#?}");
+
+    /*
     match tx_result {
         Ok(events) => {
             let auction = events
@@ -93,6 +118,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         Err(e) => Err(e)?,
     }
+    */
 
     let mut auctions = api.storage().task_auction().auctions_iter(None).await?;
     while let Some(auc) = auctions.next().await? {
